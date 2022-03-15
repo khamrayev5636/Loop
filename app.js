9c8940f2dbdbca42c70058a4a2ddd0c52d43f05b
Jamshid <jamik@.com>
2022-03-15 22:31:14 +0500

    loop

---
+++ app.js
@@ -0,0 +1,94 @@
+// let i = 0;
+// while ( i < 100) {
+//     console.log(i);
+//     i++
+// }
+
+// ==========================================================================================================
+
+// let x;
+
+// do {
+//   x = +prompt('Son kiriting');
+// } while (x != 2){
+//     console.log('2 sonini kiritmang');
+// }
+
+// =============================================================================================================
+
+// do {
+//      x = +prompt('Son kiriting');
+// } while (isNaN(x))
+
+// let javob = 1;
+
+// for( let i = 1; i <= x; i++ ){
+//      javob = javob * i
+// }
+
+
+// console.log(javob)
+
+
+
+// ============================================================================================
+
+// 1-Mashq
+
+// let x = +prompt('Son kiriting');
+
+// if(isNaN(x)){
+//     console.log('Son kiriting');
+// }else {
+//     let sum = 0
+//     let sam = 0
+//     for( let i = 1; i <= x; i++){
+//         if( i % 2 == 0){
+//             sum = sum + i
+//             console.log(i + ' Juft sonlar');
+//         }else if( i % 2 == 1){
+//             summer = sam + i                 
+//             console.log(i + ' Toq sonlar');
+//         }
+//     }
+//     console.log(sum + ' Juft sonlar yigindisi');
+//     console.log(sam + ' Toq sonlar yigindisi');
+// }
+
+
+// 2-mahshq =======================================================================
+
+
+
+// let x;
+
+// do {
+//     x = +prompt('Son kiriting');
+//     if(isNaN(x)){
+//         alert('Iltimos son kiritingqw');
+//     }else if( x % 2 == 0){
+//         alert('Juft son kiritdingiz');
+//     }else{
+//         alert('Toq son Kiritdingiz');
+//     }
+// } while('')
+
+
+// 3-mashq ===================================================================
+
+// let number = +prompt('Son kiriting');
+// let level = +prompt('Darajani kiriting');
+
+// if(isNaN(number) && isNaN(level)){
+//     alert('NAN');
+// }else{
+//     let answer = 1;
+
+// for( let i = 0; i < level; i++){
+//     answer = answer * number;
+// }
+//    alert(answer);  
+// }
+
+
+
